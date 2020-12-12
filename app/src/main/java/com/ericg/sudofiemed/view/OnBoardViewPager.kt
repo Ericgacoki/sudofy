ef4adbdee58b84942fe75381d6a980794cf3b7b2
Ericgacoki <gacokieric@gmail.com>
2020-12-12 19:55:33 +0300

    added viewpager2

---
+++ app/src/main/java/com/ericg/sudofiemed/view/OnBoardViewPager.kt
@@ -0,0 +1,38 @@
+package com.ericg.sudofiemed.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.ericg.sudofiemed.R
+import com.ericg.sudofiemed.adapter.ViewPager2Adapter
+import com.ericg.sudofiemed.databinding.FragmentOnBoardViewPagerBinding
+
+class OnBoardViewPager : Fragment() {
+    private var onBoardViewPagerBinding: FragmentOnBoardViewPagerBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragments = arrayListOf(OnBoard1(), OnBoard2(), OnBoard3())
+        val viewPagerAdapter = ViewPager2Adapter(
+            fragments,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+        onBoardViewPagerBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_on_board_view_pager, container, false)
+        return onBoardViewPagerBinding?.root?.apply {
+            onBoardViewPagerBinding?.onBoardViewPager2?.let {
+                it.adapter = viewPagerAdapter
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        onBoardViewPagerBinding = null
+    }
+}
