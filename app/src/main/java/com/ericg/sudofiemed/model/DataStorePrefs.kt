21136b58870ef331d4993b0b10bd366655ab7e09
Ericgacoki <gacokieric@gmail.com>
2020-12-12 19:57:47 +0300

    added data store preferences

---
+++ app/src/main/java/com/ericg/sudofiemed/model/DataStorePrefs.kt
@@ -0,0 +1,32 @@
+package com.ericg.sudofiemed.model
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.map
+import com.ericg.sudofiemed.model.DataStorePrefsType as PrefsType
+
+class DataStorePrefs(context: Context) {
+
+    private val dataStorePrefs = context.createDataStore(name = "app_prefs")
+
+    companion object {
+        val SHOW_ON_BOARD_KEY = preferencesKey<Boolean>("SHOW_ON_BOARD")
+    }
+
+    suspend fun setPrefs(prefType: PrefsType, value: Boolean?) {
+        if (prefType == PrefsType.SHOW_ON_BOARD) {
+            dataStorePrefs.edit {
+                value?.let { value ->
+                    it[SHOW_ON_BOARD_KEY] = value
+                }
+            }
+        }
+    }
+
+    val showOnBoard = dataStorePrefs.data.map {
+        it[SHOW_ON_BOARD_KEY] ?: true
+    }.asLiveData()
+}
---
+++ app/src/main/java/com/ericg/sudofiemed/model/DataStorePrefsType.kt
@@ -0,0 +1,3 @@
+package com.ericg.sudofiemed.model
+
+enum class DataStorePrefsType { SHOW_ON_BOARD }
