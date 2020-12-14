359e4ee116f8c045fef60754158b6b079aca500f
Ericgacoki <gacokieric@gmail.com>
2020-12-14 17:57:27 +0300

    added drag event listener to viewpager2

--- app/src/main/java/com/ericg/sudofiemed/view/OnBoardViewPager.kt
+++ app/src/main/java/com/ericg/sudofiemed/view/OnBoardViewPager.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 import com.ericg.sudofiemed.R
 import com.ericg.sudofiemed.adapter.ViewPager2Adapter
 import com.ericg.sudofiemed.databinding.FragmentOnBoardViewPagerBinding
+import com.ericg.sudofiemed.extensions.Extensions
+import com.ericg.sudofiemed.extensions.Extensions.toast
 
 class OnBoardViewPager : Fragment() {
     private var onBoardViewPagerBinding: FragmentOnBoardViewPagerBinding? = null
@@ -23,10 +25,21 @@ class OnBoardViewPager : Fragment() {
             requireActivity().supportFragmentManager,
             lifecycle
         )
-        onBoardViewPagerBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_on_board_view_pager, container, false)
+        onBoardViewPagerBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_on_board_view_pager,
+            container,
+            false
+        )
         return onBoardViewPagerBinding?.root?.apply {
             onBoardViewPagerBinding?.onBoardViewPager2?.let {
                 it.adapter = viewPagerAdapter
+                it.setOnDragListener { view, event ->
+                    if (event.x > 1){
+                        toast("+ve event captured", Extensions.ToastDuration.LONG)
+                    }
+                    true
+                }
             }
         }
     }
