0b5465d7626dd6a658fba20faade46fb246b3a26
Ericgacoki <gacokieric@gmail.com>
2021-01-06 07:41:05 +0300

    OnBoard3.kt enhancements

--- app/src/main/java/com/ericg/sudofiemed/view/OnBoard3.kt
+++ app/src/main/java/com/ericg/sudofiemed/view/OnBoard3.kt
@@ -9,8 +9,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ericg.sudofiemed.R
+import com.ericg.sudofiemed.databinding.AgreementDialogBinding
 import com.ericg.sudofiemed.databinding.FragmentOnBoard3Binding
+import com.ericg.sudofiemed.extensions.Extensions
+import com.ericg.sudofiemed.extensions.Extensions.toast
+import com.ericg.sudofiemed.model.DataStorePrefs
+import com.ericg.sudofiemed.model.DataStorePrefsType
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class OnBoard3 : Fragment() {
     private var onBoard3Binding: FragmentOnBoard3Binding? = null
@@ -27,16 +35,33 @@ class OnBoard3 : Fragment() {
         return onBoard3Binding?.root.apply {
             onBoard3Binding?.btnDoneOnBoard?.setOnClickListener {
                 showAgreementDialog()
+
             }
         }
     }
 
     private fun showAgreementDialog() {
+        val dialogObject: AgreementDialogBinding = AgreementDialogBinding.inflate(layoutInflater,null,false)
+        dialogObject.btnProceed.let {
+            it.setOnClickListener {
+                if (dialogObject.termsCheckBox.isChecked && dialogObject.analyticsCheckBox.isChecked) {
+                    proceedToFirstAids()
+                } else toast("please check all boxes", Extensions.ToastDuration.SHORT)
+            }
+        }
+
         val dialog = AlertDialog.Builder(requireContext()).create()
         dialog.apply {
             setView(layoutInflater.inflate(R.layout.agreement_dialog, null))
             window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         }.show()
+    }
+
+    private fun proceedToFirstAids() {
+        GlobalScope.launch {
+            DataStorePrefs(requireContext()).setPrefs(DataStorePrefsType.SHOW_ON_BOARD, false)
+        }
+        findNavController().navigate(R.id.action_onBoardViewPager_to_firstAids)
     }
 
     override fun onDestroy() {
