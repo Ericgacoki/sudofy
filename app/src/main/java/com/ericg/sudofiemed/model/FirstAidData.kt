8c03a0890f57d402bc906961ba8eb71d4ec40c4a
Ericgacoki <gacokieric@gmail.com>
2021-01-22 10:02:09 +0300

    bulk commits

---
+++ app/src/main/java/com/ericg/sudofiemed/model/FirstAidData.kt
@@ -0,0 +1,9 @@
+package com.ericg.sudofiemed.model
+
+import android.graphics.drawable.Drawable
+
+data class FirstAidData(
+    val title: String,
+    val image: Drawable?,
+    val desc: String
+)
