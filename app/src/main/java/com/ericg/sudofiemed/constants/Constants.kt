cbc92f935b0c3174e761ca9d4b2d66157014553a
Ericgacoki <gacokieric@gmail.com>
2021-01-22 09:58:07 +0300

    commit

--- app/src/main/java/com/ericg/sudofiemed/constants/Constants.kt
+++ app/src/main/java/com/ericg/sudofiemed/constants/Constants.kt
@@ -5,5 +5,10 @@ object Constants {
     const val requestCode = 0
     val permissions: Array<String> = arrayOf(
         android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.CAMERA
     )
 }
+
+
+
