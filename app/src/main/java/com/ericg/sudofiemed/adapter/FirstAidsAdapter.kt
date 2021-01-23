de6dbc0dfb597461720ef8c3cbe06287a751e710
Ericgacoki <gacokieric@gmail.com>
2021-01-23 10:02:53 +0300

    bulk commits

--- app/src/main/java/com/ericg/sudofiemed/adapter/FirstAidsAdapter.kt
+++ app/src/main/java/com/ericg/sudofiemed/adapter/FirstAidsAdapter.kt
@@ -3,56 +3,47 @@ package com.ericg.sudofiemed.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.ericg.sudofiemed.R
 import com.ericg.sudofiemed.databinding.FirstAidItemBinding
-import com.ericg.sudofiemed.model.FirstAid
+import com.ericg.sudofiemed.model.FirstAidData
 
 class FirstAidsAdapter(
-
-    var firstAidsList: ArrayList<FirstAid>,
+    var firstAidsListData: ArrayList<FirstAidData>,
     private val itemClick: ItemClick
 
 ) : RecyclerView.Adapter<FirstAidsAdapter.FirstAidViewHolder>() {
 
-    private var firstAidItemBinding: FirstAidItemBinding? = null
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FirstAidViewHolder {
 
-        firstAidItemBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.first_aid_item,
-            parent,
-            false
-        )
         return FirstAidViewHolder(
-            firstAidItemBinding!!
+            FirstAidItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: FirstAidViewHolder, position: Int) {
-        holder.bind(firstAidsList[position])
+        holder.bind(firstAidsListData[position])
     }
 
-    override fun getItemCount(): Int = firstAidsList.size
+    override fun getItemCount(): Int = firstAidsListData.size
 
-    inner class FirstAidViewHolder(view: FirstAidItemBinding) : RecyclerView.ViewHolder(view.root),
+    inner class FirstAidViewHolder(val view: FirstAidItemBinding) :
+        RecyclerView.ViewHolder(view.root),
         View.OnClickListener {
-        private val item = view.root
+        private val item: View = view.root
 
         init {
             item.setOnClickListener(this)
         }
 
-        fun bind(firstAid: FirstAid) {
-            firstAidItemBinding!!.firstAidName.text = firstAid.title
+        fun bind(firstAidData: FirstAidData) {
+            view.firstAidImage.setImageDrawable(firstAidData.image)
+            view.firstAidImage.clipToOutline = true
+            view.firstAidName.text = firstAidData.title
         }
 
         override fun onClick(itemView: View?) {
             itemClick.clickedItemDetails(itemView, adapterPosition)
         }
-
     }
 
     interface ItemClick {
