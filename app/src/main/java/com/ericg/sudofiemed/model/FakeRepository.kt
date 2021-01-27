813e6c791b064a6e1473368914e78623b48c453c
Ericgacoki <gacokieric@gmail.com>
2021-01-27 16:12:22 +0300

    added more files

---
+++ app/src/main/java/com/ericg/sudofiemed/model/FakeRepository.kt
@@ -0,0 +1,42 @@
+package com.ericg.sudofiemed.model
+
+import androidx.fragment.app.Fragment
+import com.ericg.sudofiemed.R
+
+object FakeRepository {
+
+    /*fun Fragment.getData(): ArrayList<FirstAidData> = arrayListOf(
+        FirstAidData("Chock", R.drawable.ic_image_place_holder, getString(R.string.desc_chock)),
+        FirstAidData("Nose breed", R.drawable.ic_image_place_holder, getString(R.string.desc_nose_breed)),
+        FirstAidData("Head ache", R.drawable.ic_image_place_holder, getString(R.string.desc_head_ache)),
+        FirstAidData("Food poisoning", R.drawable.chock, getString(R.string.desc_food_poisoning)),
+        FirstAidData("Cut", R.drawable.ic_image_place_holder, getString(R.string.desc_cut)),
+        FirstAidData("Heartburn", R.drawable.ic_image_place_holder, getString(R.string.desc_heart_burn)),
+        FirstAidData("Fire", R.drawable.ic_image_place_holder, getString(R.string.desc_fire)),
+        FirstAidData("Fracture", R.drawable.ic_image_place_holder, getString(R.string.desc_fracture)),
+        FirstAidData("Fire", R.drawable.ic_image_place_holder, getString(R.string.desc_fire))
+    )*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
